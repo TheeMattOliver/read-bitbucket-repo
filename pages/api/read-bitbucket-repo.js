c1fdbdbf8d351bcb7bb5a7cdf09a69a546a9dcbe
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-10 16:20:16 -0500

    initial commit

---
+++ pages/api/read-bitbucket-repo.js
@@ -0,0 +1,43 @@
+const { Bitbucket } = require("bitbucket");
+import fetch from "node-fetch";
+
+const clientOptions = {
+  baseUrl: "https://api.bitbucket.org/2.0",
+  request: {
+    timeout: 20,
+  },
+  auth: {
+    username: process.env.BITBUCKET_USERNAME,
+    password: process.env.BITBUCKET_SECRET,
+  },
+};
+const bitbucket = new Bitbucket(clientOptions);
+
+async function readRepo() {
+  const currentUser = await bitbucket.users.getAuthedUser({});
+  if (!currentUser.data.account_id) return false;
+  try {
+    const { data } = await bitbucket.repositories.listPermissions({});
+    const permission = data.values?.[0]?.permission;
+
+    const canWrite = !!(permission === "admin" || "write");
+    return !!canWrite;
+  } catch (e) {
+    return false;
+  }
+}
+
+export default function handler(req, res) {
+  const currentUser = bitbucket.users.getAuthedUser({});
+  if (!currentUser.data.account_id) return false;
+  try {
+    const { data } = bitbucket.repositories.listPermissions({});
+    const permission = data.values?.[0]?.permission;
+
+    const canWrite = !!(permission === "admin" || "write");
+    res.status(200).json({ currentUser: currentUser.data?.account_id });
+    return !!canWrite;
+  } catch (e) {
+    return false;
+  }
+}
