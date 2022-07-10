8c4c858bad84b14741e77b9d38b2251a2680e026
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-07-10 16:40:19 -0500

    repro

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 ## Read bitbucket repo
 
 API Endpoint to demonstrate how to read full contents of a Bitbucket repo
+
+1. Create App Password
+2. Add App Password + Bitbucket user name to .env.example and rename to `.env.local`
+3. `npm run dev`
+4. navigate to `localhost:300/api/read-bitbucket-repo`
--- pages/api/read-bitbucket-repo.js
+++ pages/api/read-bitbucket-repo.js
@@ -14,22 +14,12 @@ const clientOptions = {
 const bitbucket = new Bitbucket(clientOptions);
 
 async function readRepo() {
-  const currentUser = await bitbucket.users.getAuthedUser({});
-  if (!currentUser.data.account_id) return false;
-  try {
-    const { data } = await bitbucket.repositories.listPermissions({});
-    const permission = data.values?.[0]?.permission;
-
-    const canWrite = !!(permission === "admin" || "write");
-    return !!canWrite;
-  } catch (e) {
-    return false;
-  }
+  // TODO
 }
 
 export default async function handler(req, res) {
   const currentUser = await bitbucket.users.getAuthedUser({});
-  console.log({ currentUser });
+
   try {
     const { data } = bitbucket.repositories.listPermissions({});
     const permission = data.values?.[0]?.permission;
